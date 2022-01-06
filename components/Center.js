8137e07e05d26bb012f26007a40f484e120515db
Keith Gordon <a.keithgordon27@gmail.com>
2022-01-06 17:12:09 -0500

    Center component added; removed and replace signout button

--- components/Center.js
+++ components/Center.js
@@ -1,9 +1,11 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { playlistIdState } from "../atoms/playlistAtom";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 
 const colors = [
   "from-indigo-500",
@@ -17,18 +19,30 @@ const colors = [
 
 function Center() {
   const {data: session} = useSession();
+  const spotifyApi = useSpotify();
   const [color, setColor] = useState(null);
   const playlistId = useRecoilValue(playlistIdState);
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
 
   useEffect(() => {
     setColor(shuffle(colors).pop());
-  }, [playlistId])
+  }, [playlistId]);
+
+  useEffect(() => {
+    spotifyApi.getPlaylist(playlistId).then((data) => {
+      setPlaylist(data.body);
+    })
+    .catch((err) => console.log("Something went wrong!", err));
+  }, [spotifyApi, playlistId]);
+  
+  console.log(playlist);
+
   return (
-    <div className="flex-grow">
+    <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bgt-black space-x-3 
+        <div className="flex items-center bg-black space-x-3 
         opacity-90 hover:opacity-80 cursor-pointer rounded-full
-        p-1 pr-2">
+        p-1 pr-2 text-white" onClick={signOut}>
           <img className="rounded-full w-10" 
           src={session?.user.image} alt="" />
           <h2>{session?.user.name}</h2>
@@ -36,11 +50,19 @@ function Center() {
         </div>
       </header>
       <section className={`flex items-end space-x-7
-      bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}>
-        {/* <img src="" alt="" /> */}
+      bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
+        <img className="h-44 w-44 shadow-2xl" src={playlist?.images?.[0]?.url} alt="" />
+        <div>
+          <p>PLAYLIST</p>
+          <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
+        </div>
       </section>
+
+      <div>
+        <Songs />
+      </div>
     </div>
   )
 }
 
-export default Center
+export default Center;
