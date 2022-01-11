1969ada755964810581dfd77d069c40067b4270b
Keith Gordon <a.keithgordon27@gmail.com>
2022-01-11 11:07:30 -0500

    add volume control; finish build

--- components/Player.js
+++ components/Player.js
@@ -1,7 +1,8 @@
-import { ReplyIcon } from "@heroicons/react/outline";
-import { FastForwardIcon, PauseIcon, PlayIcon, RewindIcon, SwitchHorizontalIcon } from "@heroicons/react/solid";
+import { ReplyIcon, VolumeOffIcon } from "@heroicons/react/outline";
+import { FastForwardIcon, PauseIcon, PlayIcon, RewindIcon, SwitchHorizontalIcon, VolumeUpIcon } from "@heroicons/react/solid";
+import { debounce } from "lodash";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSongInfo";
@@ -50,6 +51,19 @@ function Player() {
   }
  },[currentTrackIdState, spotifyApi, session]);
 
+ useEffect(() => {
+  if (volume > 0 && volume < 100){
+    debouncedAdjustVolume(volume);
+  }
+ }, [volume]);
+
+ const debouncedAdjustVolume = useCallback(
+   debounce((volume) => {
+     spotifyApi.setVolume(volume).catch((err) => {});
+   }, 500),
+   [],
+ );
+
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs
     md:text-base px-2 md:px-8">
@@ -80,6 +94,15 @@ function Player() {
         className="button" 
       />
       <ReplyIcon className="button" />
+     </div>
+      {/* Right */}
+     <div className="flex items-center space-x-3
+     md:space-x-4 justify-end pr-5">
+        <VolumeOffIcon onClick={() => volume > 0 && setVolume(volume - 10)}
+        className="button" />
+          <input className="w-14 md:w-28 " type="range" value={volume}
+          onChange={e => setVolume(Number(e.target.value))} min={0} max={100} />
+        <VolumeUpIcon onClick={() => volume < 100 && setVolume + 10} className="button" />
      </div>
     </div>
   )
